ec659a86038181aca3b3dd870ebd4730ff4af4bc
Erick Galileo Padilla <erick.galileo@outlook.com>
2022-10-18 16:13:34 -0600

    creacion de enunciado

---
+++ Matrices4-EquisCero/src/matrices4/equiscero/Matrices4EquisCero.java
@@ -0,0 +1,23 @@
+
+package matrices4.equiscero;
+
+/*
+  @author Gali :B
+
+    Elaborar una aplicacion de Java with Ant, el cual realice el juego de 
+    "equis - cero" usando matrices, debe considerar lo siguiente:
+
+      * Se jugará un usuario a la vez.
+      * Se mostrará el tablón despues de cada jugada (usando JTextArea).
+      * No puede sobreescribir jugada de ningun jugador 
+        (La casilla que esté ocupada ya no se puede modificar).
+      * Hacer validaciones estrictas para cada movimiento.
+      * Cuando un jugador de los dos gane, terminará el juego.
+
+ */
+public class Matrices4EquisCero {
+    public static void main(String[] args) {
+        
+    }
+    
+}
